7e1ae826023f3cca4ae1ab1af4ad817dc8d339f0
LinusJacobsson <jacobssonlinus00@gmail.com>
2026-05-28 20:42:42 +0200

    more lint

--- src/trading_bot/core/config.py
+++ src/trading_bot/core/config.py
@@ -1,5 +1,6 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
 
 class Settings(BaseSettings):
     odds_api_key: str = Field(default=...)
